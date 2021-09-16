b37781e398ca46241b1049e2cf7f9bd66a83423b
sgarcia4 <sgarcia4@uiowa.edu>
2021-09-15 23:49:00 -0500

    HW1 Submission

--- src/edu/uiowa/cs/warp/WorkLoadDescription.java
+++ src/edu/uiowa/cs/warp/WorkLoadDescription.java
@@ -1,12 +1,19 @@
-/**
+/*
+ * WorkLoadDescription
  * 
+ * Created by Steve Goddard
+ * Modified by Steven Garcia on 9/15/2021
  */
 package edu.uiowa.cs.warp;
 
+import java.util.*;
+
 /**
+ * Provides information on graphs that are given through .txt files
+ * 
  * @author sgoddard
- *
  */
+
 public class WorkLoadDescription extends VisualizationObject {
 
 	private static final String EMPTY = "";
@@ -53,5 +60,45 @@ public class WorkLoadDescription extends VisualizationObject {
 		inputGraphString = gf.readGraphFile(inputFile);
 		this.inputFileName = gf.getGraphFileName();
 		description = new Description(inputGraphString);
+	}
+	
+	public static void main(String[] args) {
+				
+		//Declarations at the beginning of block 
+		int REMOVE_CURLY = 3; //constant to remove curly bracket
+		int FLOW_NUM = 1; //used to increment printed flow numbers
+		String Temp; //temporary string to hold graph title 
+		String graphToString; //used to manipulate file contents
+		String[] fileLines; //temporary array to hold contents
+		ArrayList<String> sortingList = new ArrayList<>(); //ArrayList used to sort
+		
+		WorkLoadDescription inputGraph = new WorkLoadDescription("StressTest4.txt"); 
+		
+		graphToString = inputGraph.toString(); 
+		
+		fileLines = graphToString.split("\n"); //each line becomes its own element in array
+				
+		//add graph contents to ArrayList
+		for(int i = 0; i < fileLines.length; i ++) {
+			
+			sortingList.add(fileLines[i]);
+			
+		}
+		
+		Temp = sortingList.remove(0); //remove brackets and title to focus on sorting
+		
+		sortingList.remove(sortingList.size() - 1 );
+		
+		Collections.sort(sortingList); //sort
+		//Collections.sort(sortingList, String.CASE_INSENSITIVE_ORDER);
+		
+		//print graph title followed by sorted contents
+		System.out.println(Temp.substring(0, Temp.length() - REMOVE_CURLY));
+		
+		for(int k = 0; k < sortingList.size(); k ++) {
+			
+			System.out.print("Flow " + (k + FLOW_NUM) + ": " + sortingList.get(k));
+		
+		}
 	}
 }
