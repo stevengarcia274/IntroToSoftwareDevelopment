ec03f8a8e69bbec0f45d336fa2e34e9cc24e39ef
sgarcia4 <sgarcia4@uiowa.edu>
2021-10-12 17:19:38 -0500

    Added method to ReliabilityAnalysis class box

--- src/edu/uiowa/cs/warp/ReliabilityAnalysis.java
+++ src/edu/uiowa/cs/warp/ReliabilityAnalysis.java
@@ -40,12 +40,20 @@ package edu.uiowa.cs.warp;
  */
 public class ReliabilityAnalysis {
 	// TODO Auto-generated class
+
+
       
     
     public ReliabilityAnalysis(Program program) {
 		// TODO Auto-generated constructor stub
 	}
 
+    // TODO Auto-generated class
+    public ReliabilityTable getReliabilities() {
+    	   // TODO implement this operation
+    	   throw new UnsupportedOperationException("not implemented");
+    	}
+    
 	public Boolean verifyReliabilities ()  {
 		// TODO Auto-generated method stub
         return false;
