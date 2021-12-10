83659e1fc7d05b31dccbc3c7e7f4831605ab9141
sgarcia4 <sgarcia4@uiowa.edu>
2021-12-10 12:56:10 -0600

    transfer file projects over

--- README.md
+++ README.md
@@ -17,3 +17,43 @@ Student Edit (10/27/21): Refactored nTxPerLinkAndTotalTxCost and getFixedTxPerLi
 constructors were also made, JavaDocs generated, and UML diagrams updated. 
 
 <br>
+Sprint 1 Summary: 
+This sprint we focused on getting a better understanding of the code. After meeting with Professor Goddard, we decided 
+that the way to go was to produce as many artifacts as possible to represent our comprehension and give in idea to the 
+direction we are headed for Sprint Two. Our goal for the next sprint is to implement ReliabilityVisualization.java, as 
+as you can see from our Reliability UML diagram, we've began thinking on how we might achieve this by adding a couple 
+of the methods we feel might be crucial.<br>
+
+Some of the files we included/updated with this sprint are:<br>
+-ReliabilityVisualization_Garcia.png<br>
+-ReliabilityAnalysis_Sequence_Diagram.png<br>
+-raFileGeneration_Morris.png<br>
+-SequenceDiagram_Culkeen.png<br>
+-raFileGenerationNotes_Morris.txt (Notes from our meeting w/ Professor Goddard)<br>
+-Reliability.umlcd<br>
+-ReliabilityVisualization.java<br>
+
+
+Sprint 2 Summary:
+This sprint we started by implementing ReliabilityVisualization.java; createHeader() and visualization() are working as of now,
+which can be seen by some of test cases we have made. In ReliabilityAnalysis.java the getReliabilities() function was implemented
+so that it now creates a dummy table filled with 0.0s We also added a ReliabilityTest.java file, with some unit tests to 
+validate that the numTxPerLinkAndTotalCost() and getHeader() functions are returning proper values. We also added a test case for
+visualization(), but as of now it does not pass because we've only implemented a dummy table, we expect that once we work out
+algorithms it should pass as planned. JavaDocs were also generated to document the methods we have implemented. Our goal for 
+the next sprint will probably include figuring out the algorithm to produce the correct values in our ReliabilityTable and 
+implementing verifyReliabilities(). 
+
+Some of the files we included/updated/added with this sprint are:<br>
+-ReliabilityVisualization.java<br>
+-ReliabilityAnalysis.java<br>
+-ReliabilityTest.java<br>
+-newly generated JavaDocs<br>
+<br>
+
+Sprint 3 Summary:<br>
+*Final summary to be inserted here* <br>
+<br>
+- updated UML Diagram to include updated components of ReliabilityAnalysis
+and ReliabilityVisualization<br>
+-added javadoc comments to functions in ReliabilityAnalysis<br>
--- src/edu/uiowa/cs/warp/ReliabilityVisualization.java
+++ src/edu/uiowa/cs/warp/ReliabilityVisualization.java
@@ -1,5 +1,7 @@
 package edu.uiowa.cs.warp;
 
+import java.util.ArrayList;
+
 /**
  * ReliabilityVisualization creates the visualizations for
  * the reliability analysis of the WARP program. <p>
@@ -12,17 +14,111 @@ package edu.uiowa.cs.warp;
  */
 public class ReliabilityVisualization  extends VisualizationObject {
 
-	// TODO Auto-generated class stub for unimplemented visualization
 	
 	private static final String SOURCE_SUFFIX = ".ra";
 	private static final String OBJECT_NAME = "Reliability Analysis";
 	private Warp warp;
 	private ReliabilityAnalysis ra;
 	
+	/**
+	 *	This is the constructor for the ReliabilityVisualization object,
+	 *	which takes a Warp object and stores it, then runs 
+	 *	ReliabilityAnalysis on it.
+	 *
+	 *	@param Warp Warp object passed in
+	 */
+	
 	ReliabilityVisualization(Warp warp) {
 		super(new FileManager(), warp, SOURCE_SUFFIX);
 		this.warp = warp;
 		this.ra = warp.toReliabilityAnalysis();
+	}
+	
+	/**
+	 * The public method visualization takes the 
+	 * stored information from the ReliabilityVisualization object
+	 * and uses it to generate a table visualization of the stored
+	 * ReliabilityAnalysis data.
+	 * 
+	 * @return content The formatted ReliabilityAnalysis data
+	 */
+	@Override
+	public Description visualization() {
+		Description content = new Description();
+		int REMOVE_LAST_TAB = 1;
+		var warpToPro = warp.toProgram();
+		
+		//var orderedNodes = warpToPro.toWorkLoad().getNodesOrderedAlphabetically(); 
+		//var orderedFlows = warpToPro.toWorkLoad().getFlowNames();
+		var orderedFlows = warpToPro.toWorkLoad().getFlowNamesInPriorityOrder();
+		String flowString = "";
+		//create column names that should be in the format of flowName:node
+		for(int i = 0; i < orderedFlows.size(); i++) {
+			var nodesIn = warpToPro.toWorkLoad().getNodesInFlow(orderedFlows.get(i));
+			for(int t = 0; t < nodesIn.length; t++) {
+				flowString += String.format("%s:%s\t", orderedFlows.get(i), nodesIn[t]);
+				
+			}
+			
+		}
+		
+		flowString = flowString.substring(0, flowString.length() - REMOVE_LAST_TAB) + "\n";
+		content.add(flowString);
+		
+		//generate reliabilityTable that will be displayed in the file
+    	ReliabilityTable insertTable = new ReliabilityTable();
+    	insertTable = ra.getReliabilities();
+    	//parse through the rows of reliability table and add them to the string
+    	//so they can be displayed in the file
+    	for(int rowIndex = 0; rowIndex < insertTable.size(); rowIndex++) {
+    		String addString = "";
+    		ReliabilityRow rowArrayList = insertTable.get(rowIndex);
+    		for(int i = 0; i < rowArrayList.size(); i++){
+    			if(i != (rowArrayList.size() - 1)) {
+    				addString = addString + rowArrayList.get(i) + "\t";
+    			}else {
+    				addString = addString + rowArrayList.get(i); 
+    			}
+    		}
+    		//addString = addString + rowArrayList + "\n";
+    		addString = addString + "\n";
+    		content.add(addString);		
+    	}
+		
+    	return content;
+	}
+
+	/**
+	 * This createHeader method is an Override of the one 
+	 * found in the VisualizationObject class, with this
+	 * one being refactored to specifically generate headers
+	 * for .ra files, getting the graph name, scheduler name,
+	 * minimum packet reception rate, number of channels,and 
+	 * number of transmissions.
+	 * 
+	 * @return header The formatted header information returned as a Description object
+	 */
+	@Override
+	public Description createHeader() {
+		Description header = new Description();
+		
+		header.add(String.format(
+				"Reliability Analysis for graph %s created with the following parameters:\n",
+				warp.getName()));
+    	header.add(String.format(
+    			"Scheduler Name:\t%s\n",warp.getSchedulerName()));
+    	header.add(String.format(
+    			"M:\t%s\n",String.valueOf(warp.getMinPacketReceptionRate())));
+    	header.add(String.format("E2E:\t%s\n",String.valueOf(warp.getE2e())));
+    	header.add(String.format("nChannels:\t%d\n",warp.getNumChannels()));
+    	/* The following parameters are only output when special schedules are requested */
+    	if (warp.getNumTransmissions() > 0) { // only specify when NumTransmissions is fixed
+    		header.add(String.format("nTransmissions:\t%d)\n",warp.getNumTransmissions()));
+    	}
+    	if (!warp.getOptimizationFlag()) { // only specify when optimization not requested
+    		header.add("Optimization Requested:\tFalse\n") ;
+    	}
+		return header;
 	}
 	
 /* File Visualization for workload defined in Example.txt follows. Note
@@ -37,11 +133,11 @@ M:	0.9
 E2E:	0.99
 nChannels:	16
 F0:A	F0:B	F0:C	F1:C	F1:B	F1:A
-1.0	0.9	0.0	1.0	0.0	0.0
-1.0	0.99	0.81	1.0	0.0	0.0
-1.0	0.999	0.972	1.0	0.0	0.0
-1.0	0.999	0.9963	1.0	0.0	0.0
-1.0	0.999	0.9963	1.0	0.9	0.0
+1.0	0.9	0.0	1.0 0.0 0.0
+1.0	0.99	0.81	1.0	0.0 	0.0
+1.0	0.999	0.972	1.0	0.0 	0.0
+1.0	0.999	0.9963	1.0	0.0 	0.0
+1.0	0.999	0.9963	1.0	0.9 	0.0
 1.0	0.999	0.9963	1.0	0.99	0.81
 1.0	0.999	0.9963	1.0	0.999	0.972
 1.0	0.999	0.9963	1.0	0.999	0.9963
